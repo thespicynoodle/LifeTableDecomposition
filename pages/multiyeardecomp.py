1cce139859802284ca261a43aa104794435e2ab2
thespicynoodle <brandon.lauton13@gmail.com>
2024-09-16 20:06:12 +1000

    Update multiyeardecomp.py
    remove life difference from tab4

--- pages/multiyeardecomp.py
+++ pages/multiyeardecomp.py
@@ -456,8 +456,6 @@ with tab4:
         st.dataframe(pivot_df)
 
 
-        total_difference = contribution_df[contribution_df['Age'] == 'Life expectancy difference']['Contribution to LE difference (years)'].values[0]
-        st.write(f"**Total Life Expectancy Difference from {year1} to {year2}:** {total_difference:.4f} years")
         # Exclude the total row for plotting
         plot_df = pivot_df[pivot_df['Age'] != 'Total']
 

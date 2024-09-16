69c2032feb6d30147f3302ac001d14e3af913141
thespicynoodle <brandon.lauton13@gmail.com>
2024-09-16 20:13:42 +1000

    Update multiyeardecomp.py

--- pages/multiyeardecomp.py
+++ pages/multiyeardecomp.py
@@ -298,7 +298,11 @@ countries = [
 ]
 
 # Define the list of years from 1990 to 2020 in 5-year intervals
-years = [1990, 1995, 2000, 2005, 2010, 2015, 2020]
+years = [1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 
+         2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 
+         2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 
+         2020, 2021]
+
 
 # Define genders (adjust according to your data)
 genders = ['Male', 'Female']
